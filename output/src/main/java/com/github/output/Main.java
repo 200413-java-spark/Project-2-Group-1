8fc747fb1e95d340d03b99e4889e9dddee914089
Sutter Grune <suttergrune@gmail.com>
2020-05-20 17:09:10 -0400

    create third module

---
+++ output/src/main/java/com/github/output/Main.java
@@ -0,0 +1,10 @@
+package com.github.output;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n Purpose of this method: process the output of the spark job and persist the results to the database \n");
+
+    }
+}
