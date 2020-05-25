8dd504d75f0317c955d21c7b2aa0f06bc9a0fd4b
Sutter Grune <suttergrune@gmail.com>
2020-05-25 16:43:10 -0400

    learning spark sql

--- spark/src/main/java/com/github/sparkBusiness/sparkSQL/SutterSQL.java
+++ spark/src/main/java/com/github/sparkBusiness/sparkSQL/SutterSQL.java
@@ -3,9 +3,10 @@ package com.github.sparkBusiness.sparkSQL;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 public class SutterSQL implements SQLInt {
-    
+
     public Dataset<Row> dataset;
     public SparkSession sparkSession;
 
@@ -16,6 +17,37 @@ public class SutterSQL implements SQLInt {
 
     @Override
     public void run() {
-
+        try {
+            execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public void execute() throws Exception {
+
+        // Dataset<Row> data = dataset.cache();
+        Dataset<Row> df = dataset;
+
+        // PRINT SCHEMA:
+        // df.printSchema();
+        
+        // PRINT DATA
+        // df.show();
+
+        // METHOD: select
+        // df.select("State").show();
+        df.select(col("County"), col("Life_Expectancy")).show(10);
+
+
+
+        // More...
+        // dataset.createOrReplaceTempView("County");
+        // dataset.select("County").show(10);
+        // Dataset<Row> count = sparkSession.sql("SELECT COUNT(*) FROM County");
+        // count.show();
+        // Dataset<Row> results = sparkSession.sql("SELECT COUNT(*) FROM counties");
+    
+    }
+
 }
--- spark/src/test/java/spark/sparkSQL/SutterSQLTest.java
+++ spark/src/test/java/spark/sparkSQL/SutterSQLTest.java
@@ -1,7 +1,9 @@
 package spark.sparkSQL;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import org.apache.spark.sql.Dataset;
@@ -10,9 +12,10 @@ import org.apache.spark.sql.SparkSession;
 
 import com.github.sparkBusiness.sparkSQL.SutterSQL;
 
+// @Ignore
 public class SutterSQLTest {
-    
-    // mvn test SutterSQLTest
+
+    // mvn clean test -Dtest=SutterSQLTest
 
     private static final String INPUT_FILE = "src/test/resources/input/life-expectancy-data.csv";
     private static final String INPUT_FILE_TEST = "src/test/resources/input/life-expectancy-data-sample.csv";
@@ -22,26 +25,27 @@ public class SutterSQLTest {
 
     @BeforeClass
     public static void createSession() {
+        sparkSession = SparkSession.builder().appName("spark-sql-sutter").master("local[*]").getOrCreate();
+        dataset = sparkSession.read().option("header", true).csv(INPUT_FILE_TEST);
+    }
 
-        // sparkSession = SparkSession
-        //     .builder()
-        //     .appName("spark-job")
-        //     .master("local[*]")
-        //     .getOrCreate();
-
-        // dataset = sparkSession
-        //     .read()
-        //     .option("header", true)
-        //     .csv(INPUT_FILE_TEST);
-
-        // sutterSQL = new SutterSQL(dataset, sparkSession);
-
+    @AfterClass
+    public static void closeSession() {
+        sparkSession.close();
     }
 
     @Test
     public void testSpark() {
-        //sutterSQL.run();
-        Assert.assertTrue(true);
+        boolean completed = false;
+        sutterSQL = new SutterSQL(dataset, sparkSession);
+        try {
+            sutterSQL.execute();
+            completed = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            completed = false;
+        }
+        Assert.assertTrue(completed);
     }
 
 }
